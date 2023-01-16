d7359fe6eddb0cd80a1def09efe9d1eb71b48ad8
Aaron Dayrit <78222918+AaronDayrit@users.noreply.github.com>
2023-01-16 13:57:30 -0600

    changed content

--- src/views/Article.js
+++ src/views/Article.js
@@ -20,7 +20,7 @@ function Article() {
                         <i class="fa-solid fa-bookmark"></i>
                     </div>
 
-                    <img src="https://www.econlib.org/wp-content/uploads/2018/04/Great-Depression.jpeg" />
+                    <img src="https://images.squarespace-cdn.com/content/v1/62015f66f840ef671da14ae7/adf352ea-3bc2-4938-b58c-79342696a6a6/Visiting+Palm+Springs58.JPG?format=1000w" />
                     <p class="caption">Image source: unplash.com</p>
                 </div>
 
@@ -31,56 +31,31 @@ function Article() {
 
                 <div className="par-container">
                     <p>
-                        The Great Depression is a point in time when humanity suffered from the worst economic downturn in history. The
-                        point that impacted the daily lives of people across the street, beyond the oceans. No one was excluded. Recently
-                        there was another disaster to this scale. Covid-19 affects all aspects of our lives. With our own two eyes, we saw
-                        the causalities and the conditions of the natural flow of society. Has history repeated itself yet again? With what
-                        we've experienced with Covid-19, have we experienced another Great Depression?
+                        Inadequacy is a feeling that many of us experience at some point in our lives. It can manifest in different ways, such as feeling like we're not good enough, smart enough, or talented enough. This feeling can be debilitating and can hold us back from reaching our full potential.
                     </p>
 
                     <p>
-                        The reduction of gross domestic product started the rippling effect in the workforce of both time periods. Work
-                        would have reduced hours at best and layoffs at worst (<span>Unemployment during the Great Depression</span>). April of 2020 had
-                        an official American unemployment rate of 14.7%, the highest since the above 25% of the Great Depression (<span>Iacurci</span>).
-                        At a time of unemployment, both situations involved government support as a response to the (<span>Stock market crash of 1929</span>).
-                        What transpired would be a period of isolation.
+                        One of the biggest causes of inadequacy is comparison. We often compare ourselves to others and measure our own worth based on how we stack up. This can be especially true in today's world, where social media and the internet make it easy to see the highlight reels of others' lives. We see their achievements and successes and compare them to our own perceived failures and shortcomings.
                     </p>
 
                     <p>
-                        The two events coincide with the immediate change in social conditions. Most individuals would most likely
-                        experience more frugal spending (<span>Thompson School District R2-J</span>). They were both times that involved making do with
-                        things you already have. The depression included budget celebrations and forms of entertainment that included
-                        Potlucks and "thrift gardens" as well as board games (<span>Thompson School District R2-J</span>). Covid involved isolation in
-                        homes, usually with roommates, family, or friends.
+                      The key to overcoming the feeling of inadequacy is to focus on yourself and your own journey. Instead of comparing yourself to others, focus on your own progress and growth. Recognize that everyone has their own unique path and that what works for one person may not work for you.
                     </p>
 
                     <p>
-                        The bleakest periods of human history often led to anger, protests, and eventual violence. The great depression
-                        brought the rise of the Unemployed Councils (<span>Foster</span>). These people brought upon the protests of the unfortunate
-                        majority who were unemployed (<span>Foster</span>). The methods they used were modern, like today. Parades, petitions, and
-                        marches were the standard (<span>Foster</span>). Police Brutality was normal (<span>Foster</span>) which is still an issue today. The Covid
-                        Pandemic was no different in this regard. Protests were the result of disproval of groups of people against mandates
-                        of masks and restrictions to reduce the spread by the government (<span>Key events in Canada’s trucker protests against
-                        Covid curbs</span>). A notable protest was Canada with the "Freedom Convoy" that included Canadian truckers and their
-                        dissatisfaction with the mandates affecting their work of transportation.
+                       Another way to overcome inadequacy is to practice self-compassion. Treat yourself with the same kindness and understanding that you would offer to a friend. Instead of beating yourself up for your mistakes and shortcomings, be kind and understanding with yourself. Recognize that everyone makes mistakes and that it's a natural part of the learning process.
                     </p>
 
                     <p>
-                        Arguably, the most striking difference between these two periods of history is the methods we used to recover from
-                        these disasters. After the Great Depression was World War Two (<span>History of Western Civilization II</span>). The war allowed
-                        the unfortunate as they were listed in the military (<span>History of Western Civilization II</span>). By then, unemployment
-                        insurance was implemented by the government <span>(History of Western Civilization II</span>). Covid didn't have a world war to
-                        lean on. The solutions to the pandemic were isolation and public cooperation. The government implements standards
-                        and bans to remove the opportunity of spread before a reliable vaccine Is produced (<span>Key events in Canada’s trucker
-                        protests against Covid curbs</span>). That plan would be nothing without much of the public agreeing. The Great Depression
-                        was solved by a government giving opportunities. The Pandemic, though still ongoing, was brought in the right
-                        direction by a government giving opportunity, and the public agrees with it.
+                        It's also important to set realistic goals for yourself. Setting unrealistic expectations can lead to feelings of inadequacy when you inevitably fall short. Instead, set realistic and achievable goals that will help you to feel a sense of accomplishment and progress.
                     </p>
 
                     <p>
-                        The Great Depression and the Covid Pandemic share prominent similarities. The cause of the world's problems of
-                        unemployment, their social conditions, and the protests and movements are all examples of those similarities. Humans
-                        often make the same mistake. History repeating is not uncommon.
+                        Finally, surround yourself with supportive people who encourage and uplift you. Surround yourself with people who believe in you and your abilities, and who will support you through your journey. This can be especially helpful when you're feeling down or inadequate.
+                    </p>
+                        In conclusion, inadequacy is a feeling that many of us experience at some point in our lives. However, it's important to remember that it's a normal part of the human experience. To overcome the feeling of inadequacy, focus on yourself, practice self-compassion, set realistic goals, and surround yourself with supportive people. With these strategies, you can overcome the feeling of inadequacy and reach your full potential.
+                    <p>
+        
                     </p>
                 </div>
 
